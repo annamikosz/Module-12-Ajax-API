e4b0da72efcfcce88e682c7cfda59f03613e33fb
Anna Mikosz <annamikosz.lu@gmail.com>
2017-06-28 21:31:25 +0200

    Fixed

--- js/script.js
+++ js/script.js
@@ -6,19 +6,20 @@ $(function () {
 
   function searchCountries() {
     var countryName = $('#country-name').val();
-  if(!countryName.length) countryName = 'Poland';
-  $.ajax({
+    if(!countryName.length) countryName = 'Poland';
+    
+    $.ajax({
     url: url + countryName,
     method: 'GET',
     success: showCountriesList
-  });
-  function showCountriesList(resp) {
-    countriesList.empty();
-  resp.forEach(function(item){
-    $('<li>').text(item.name).appendTo(countriesList);
-  });
+    });
   }
 
+  function showCountriesList(resp) {
+    countriesList.empty();
+    resp.forEach(function(item){
+      $('<li>').text(item.name).appendTo(countriesList);
+    });
   }
 
 
