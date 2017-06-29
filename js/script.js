4c7568c0f25ba629ce55a1d3e120c1c96a35e5ef
Anna Mikosz <annamikosz.lu@gmail.com>
2017-06-29 23:47:04 +0200

    final version

--- js/script.js
+++ js/script.js
@@ -18,14 +18,46 @@ $(function () {
   function showCountriesList(resp) {
     countriesList.empty();
     resp.forEach(function(item){
-  
-      $('<li>').addClass('name').text("Name: " + item.name).appendTo(countriesList);
-      $('<li>').text("Capital: "  + item.capital).appendTo(countriesList);
-      $('<li>').text("Region: "  + item.region).appendTo(countriesList);
-      $('<li>').text("Subregion: "  + item.subregion).appendTo(countriesList);
-      $('<li>').text("Language: "  + item.languages).appendTo(countriesList);
-      $('<li>').text("Timezones: "  + item.timezones).appendTo(countriesList);
 
+      $titleTableCountry = $('<h3 class="title">Background information</h3>'),
+      $row =  $('<li>').append($titleTableCountry),
+      $tableCountry = $('<table>'),
+      $rowCountryName = $('<tr class="name">'),
+      $rowCountryCapital = $('<tr>'),
+      $rowCountryRegion = $('<tr>'),
+      $rowCountrySubregion = $('<tr>'),
+      $rowCountryLanguages = $('<tr>'),
+      $rowCountryTimezones = $('<tr>'),
+
+      $cellName = $('<td>').text("Name: "),
+      $cellNameCountry = $('<td>').text(item.name),
+      $cellCapital = $('<td>').text("Capital: ").css("text-transform","uppercase"),
+      $cellCapitalCountry = $('<td>').text(item.capital),
+
+      $cellRegion = $('<td>').text("Region: ").css("text-transform","uppercase"),
+      $cellRegionCountry = $('<td>').text(item.region),
+      $cellSubregion = $('<td>').text("Subregion: " ).css("text-transform","uppercase"),
+      $cellSubregionCountry = $('<td>').text(item.subregion),
+      $cellLanguages = $('<td>').text("Languages: " ).css("text-transform","uppercase"),
+      $cellLanguagesCountry = $('<td>').text(item.languages),
+      $cellTimezones = $('<td>').text("Timezones: " ).css("text-transform","uppercase"),
+      $cellTimezonesCountry = $('<td>').text(item.timezones),
+
+      ($row).append($tableCountry).appendTo(countriesList);
+
+      ($tableCountry).append($rowCountryName)
+      .append($rowCountryCapital)
+      .append($rowCountryRegion)
+      .append($rowCountrySubregion)
+      .append($rowCountryLanguages)
+      .append($rowCountryTimezones);
+
+      ($rowCountryName).append($cellName).append($cellNameCountry);
+      ($rowCountryCapital).append($cellCapital).append($cellCapitalCountry);
+      ($rowCountryRegion).append($cellRegion).append($cellRegionCountry);
+      ($rowCountrySubregion).append($cellSubregion).append($cellSubregionCountry);
+      ($rowCountryLanguages).append($cellLanguages).append($cellLanguagesCountry);
+      ($rowCountryTimezones).append($cellTimezones).append($cellTimezonesCountry);
     });
   }
 
